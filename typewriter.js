ff9147af986df783aaa0d72c312d48ea18a4dfb1
Anthony Chambers <ajfchambers@gmail.com>
2023-03-12 15:34:26 -0400

    typewriter.js linted

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,12 @@
 let sentence = "hello there from lighthouse labs";
-let delay = 0; 
+let delay = 0;
 
-sentence += "\n"
+sentence += "\n";
 
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
-  }, delay)
+  }, delay);
 
   delay += 50;
 }
